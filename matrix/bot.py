f725390dcfc57e4de1232dea05612cfd6399d8d2
Chris <rolletchris1@gmail.com>
2025-05-28 14:32:31 -0700

    Refactor Bot class to use Context in command processing and event handling

--- matrix/bot.py
+++ matrix/bot.py
@@ -22,10 +22,9 @@ from nio import (
     TypingNoticeEvent,
 )
 
-R = TypeVar("R", bound=MatrixRoom)
-E = TypeVar("E", bound=Event)
-Callback = Callable[[R, E], Awaitable[None]]
+from matrix.context import Context
 
+Callback = Callable[[Context], Awaitable[None]]
 
 class Bot:
     EVENT_MAP: Dict[str, Type[Event]] = {
@@ -189,7 +188,8 @@ class Bot:
             if isinstance(event, event_type):
                 for func in funcs:
                     try:
-                        await func(room, event)
+                        ctx = Context(bot=self, room=room, event=event)
+                        await func(ctx)
                     except Exception:
                         self.log.exception(
                             "error in handler %s",
@@ -198,14 +198,13 @@ class Bot:
 
     async def _process_commands(
         self,
-        room: MatrixRoom,
-        event: RoomMessageText
+        ctx: Context
     ) -> None:
         """Parse and execute commands"""
-        if not self.prefix or not event.body.startswith(self.prefix):
+        if not self.prefix or not ctx.event.body.startswith(self.prefix):
             return
 
-        parts = event.body[len(self.prefix) :].split()
+        parts = ctx.event.body[len(self.prefix) :].split()
         if not parts:
             return
 
@@ -216,7 +215,7 @@ class Bot:
             return
 
         try:
-            await cmd(room, event)
+            await cmd(ctx)
         except Exception:
             self.log.exception("error while executing command '%s'", cmd_name)
 
@@ -224,7 +223,7 @@ class Bot:
         """Invoked after a successful login, before sync starts."""
         self.log.info("bot is ready")
 
-    async def on_message(self, room: MatrixRoom, event: RoomMessageText):
+    async def on_message(self, ctx: Context):
         """
         Invoked when a message event is received.
 
@@ -232,12 +231,10 @@ class Bot:
         event is detected. It is primarily responsible for detecting and processing
         commands that match the bot's defined prefix.
 
-        :param room: The Matrix room where the message was sent.
-        :type room: MatrixRoom
-        :param event: The message event received from the room.
-        :type event: RoomMessageText
+        :param ctx: The context object containing information about the Matrix room and the message event.
+        :type ctx: Context
         """
-        await self._process_commands(room, event)
+        await self._process_commands(ctx)
 
     async def run(self, user_id: str, password: str) -> None:
         """
