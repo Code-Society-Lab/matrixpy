d36331097c8b1b657b78036fe895823bfddb8670
Chris <rolletchris1@gmail.com>
2025-05-28 16:40:12 -0700

    Refactor ping command to specify command name and improve logging details

--- examples/ping.py
+++ examples/ping.py
@@ -3,9 +3,9 @@ from matrix.bot import Bot, Context
 bot = Bot("https://matrix.org", prefix="!")
 
 
-@bot.command()
+@bot.command("ping")
 async def ping(ctx: Context):
-  print(f"{ctx.event.sender} sent a message.")
+  print(f"{ctx.sender} invoked {ctx.body} in room {ctx.room_name}.")
   await ctx.send("Pong!")
 
 bot.start("user id", "password")
