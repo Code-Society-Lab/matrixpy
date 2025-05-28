6fc3db5448a017b590857b15d1a216e023abf3a3
Chris <rolletchris1@gmail.com>
2025-05-28 14:32:13 -0700

    Updated Context class to abstract room_send() action

--- matrix/context.py
+++ matrix/context.py
@@ -1,3 +1,14 @@
+from nio import Event, MatrixRoom
+
 class Context:
-    def __init__(self, bot, room, event):
-        ...
+    def __init__(self, bot, room: MatrixRoom, event: Event):
+        self.bot     = bot
+        self.room    = room
+        self.event   = event
+
+    async def send(self, message: str):
+        await self.bot.client.room_send(
+            self.room.room_id,
+            message_type="m.room.message",
+            content={"msgtype": "m.text", "body": message}
+        )
