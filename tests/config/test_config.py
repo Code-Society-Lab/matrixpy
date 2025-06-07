6f962b47e7377a6c12790f0efab7e8bd7c3e15e5
Chris <rolletchris1@gmail.com>
2025-06-06 21:32:14 -0700

    Added unittest for loading config from a file

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -16,3 +16,11 @@ def test_config_without_password_or_token():
     with pytest.raises(ConfigError) as exc:
         Config(username="missingpassword")
     assert "username and password or token" in str(exc.value)
+
+
+def test_load_from_file():
+    cfg = Config(config_path="tests/config/config_file.yaml")
+    assert cfg.homeserver == "https://matrix.org"
+    assert cfg.user_id == "grace"
+    assert cfg.password == "grace1234"
+    assert cfg.prefix == "!"
