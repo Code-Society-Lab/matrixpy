074c93bde5a9ff514bd07e0b0540c5617f9134e9
Chris <rolletchris1@gmail.com>
2025-06-12 13:59:47 -0700

    Implement Room class for managing Matrix room interactions

---
+++ matrix/room.py
@@ -0,0 +1,59 @@
+from matrix.errors import MatrixError
+from matrix.message import Message
+from typing import TYPE_CHECKING, Optional
+
+if TYPE_CHECKING:
+    from matrix.bot import Bot  # pragma: no cover
+
+
+class Room:
+    """
+    Represents a Matrix room and provides methods to interact with it.
+
+    :param room_id: The unique identifier of the room.
+    :type room_id: str
+    :param bot: The bot instance used to send messages.
+    :type bot: Bot
+    """
+
+    def __init__(self, room_id: str, bot: "Bot") -> None:
+        self.room_id = room_id
+        self.bot = bot
+
+    async def send(
+            self,
+            message: str,
+            markdown: Optional[bool] = True
+    ) -> None:
+        """
+        Send a message to the room.
+
+        :param message: The message to send.
+        :type message: str
+        :param markdown: Whether to format the message as Markdown.
+        :type markdown: bool
+
+        :raises MatrixError: If sending the message fails.
+        """
+        try:
+            room = Message(self.bot)
+            await room.send(self.room_id, message, markdown)
+        except Exception as e:
+            raise MatrixError(f"Failed to send message: {e}")
+
+    async def invite_user(self, user_id: str) -> None:
+        """
+        Invite a user to the room.
+
+        :param user_id: The ID of the user to invite.
+        :raises MatrixError: If inviting the user fails.
+        """
+        try:
+            # TODO: Abstract this to Context?
+            #       EX: await Context.invite_user_to_room(user_id)
+            await self.bot.client.room_invite(
+                room_id=self.room_id,
+                user_id=user_id
+            )
+        except Exception as e:
+            raise MatrixError(f"Failed to invite user: {e}")
