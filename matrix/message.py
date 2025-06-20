66f8722163f94d14fcd5ca7213b84185a8140a0a
Chris <rolletchris1@gmail.com>
2025-06-19 22:07:57 -0700

    Add support for message reactions and improve content creation in Message class

--- matrix/message.py
+++ matrix/message.py
@@ -1,6 +1,7 @@
 from matrix.errors import MatrixError
 import markdown
 from typing import Dict, Optional
+from nio import Event
 
 
 class Message:
@@ -47,7 +48,14 @@ class Message:
         except Exception as e:
             raise MatrixError(f"Failed to send message: {e}")
 
-    def _make_content(self, body: str, html: Optional[bool]) -> Dict:
+    def _make_content(
+        self,
+        body: str,
+        html: Optional[bool] = None,
+        reaction: Optional[bool] = None,
+        event_id: Optional[str] = None,
+        key: Optional[str] = None,
+    ) -> Dict:
         """
         Create the content dictionary for a message.
 
@@ -55,6 +63,12 @@ class Message:
         :type body: str
         :param html: Wheter to format the message as HTML.
         :type html: bool
+        :param reaction: Wheter to format the context with a reaction event.
+        :type reaction: bool
+        :param event_id: The ID of the event to react to.
+        :type event_id: str
+        :param key: The reaction to the message.
+        :type key: Optional[str]
 
         :return: The content of the dictionary.
         """
@@ -68,10 +82,18 @@ class Message:
             base["format"] = self.MATRIX_CUSTOM_HTML
             base["formatted_body"] = html_body
 
+        if reaction:
+            base["m.relates_to"] = {
+                "event_id": event_id,
+                "key": key,
+                "rel_type": "m.annotation",
+            }
+
         return base
 
     async def send(
-        self, room_id: str,
+        self,
+        room_id: str,
         message: str,
         format_markdown: Optional[bool] = True
     ) -> None:
@@ -88,5 +110,32 @@ class Message:
         """
         await self._send_to_room(
             room_id=room_id,
-            content=self._make_content(message, format_markdown),
+            content=self._make_content(body=message, html=format_markdown),
+        )
+
+    async def send_reaction(
+        self, room_id: str, event: Event, key: str
+    ) -> None:
+        """
+        Send a reaction to a message from a user in a Matrix room.
+
+        :param room_id: The ID of the room to send the message to.
+        :type room_id: str
+        :param event: The event object to react to.
+        :type event: Event
+        :param key: The reaction to the message.
+        :type key: str
+        """
+        if isinstance(event, Event):
+            event_id = event.event_id
+        else:
+            event_id = event
+            print("hi")
+
+        await self._send_to_room(
+            room_id=room_id,
+            content=self._make_content(
+                body="", event_id=event_id, key=key, reaction=True
+            ),
+            message_type="m.reaction",
         )
