7575446879b5625d8f6a1b202279ed945c5e040d
Chris <rolletchris1@gmail.com>
2025-06-22 10:17:22 -0700

    Added detailed docstrings and additional emoji reactions

--- examples/reaction.py
+++ examples/reaction.py
@@ -5,6 +5,10 @@ bot = Bot("examples/config.yaml")
 
 @bot.event
 async def on_message_react(room, event):
+    """
+    This function listens for new messages in a room and reacts based
+    on the message content.
+    """
     room = bot.get_room(room.room_id)
     if event.body.lower().startswith("thanks"):
         await room.send(event=event, key="🙏")
@@ -13,9 +17,16 @@ async def on_message_react(room, event):
         # Can also react with a text message instead of emoji
         await room.send(event=event, key="hi")
 
+    if event.body.lower().startswith("❤️"):
+        await room.send(event=event, message="❤️")
+
 
 @bot.event
 async def on_member_react(room, event):
+    """
+    This function listens for new member reaction to messages in a room,
+    and reacts based on the reaction emoji.
+    """
     room = bot.get_room(room.room_id)
 
     emoji = event.key
@@ -23,6 +34,9 @@ async def on_member_react(room, event):
 
     if emoji == "🙏":
         await room.send(event=event_id, key="❤️")
+
+    if emoji == "❤️":
+        await room.send(message="❤️")
 
 
 bot.start()
