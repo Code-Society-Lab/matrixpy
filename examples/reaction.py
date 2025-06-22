cb1fe105535c6689427a149871c347c527ee4345
Chris <rolletchris1@gmail.com>
2025-06-22 10:08:07 -0700

    Refactor reaction event handlers to improve clarity

--- examples/reaction.py
+++ examples/reaction.py
@@ -3,8 +3,8 @@ from matrix.bot import Bot
 bot = Bot("examples/config.yaml")
 
 
-@bot.event(event_spec="on_react")
-async def react_event(room, event):
+@bot.event
+async def on_message_react(room, event):
     room = bot.get_room(room.room_id)
     if event.body.lower().startswith("thanks"):
         await room.send(event=event, key="🙏")
@@ -12,6 +12,17 @@ async def react_event(room, event):
     if event.body.lower().startswith("hello"):
         # Can also react with a text message instead of emoji
         await room.send(event=event, key="hi")
+
+
+@bot.event
+async def on_member_react(room, event):
+    room = bot.get_room(room.room_id)
+
+    emoji = event.key
+    event_id = event.source["content"]["m.relates_to"]["event_id"]
+
+    if emoji == "🙏":
+        await room.send(event=event_id, key="❤️")
 
 
 bot.start()
