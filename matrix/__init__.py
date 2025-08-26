cdb6927a2221c844b0f19f77a7fba6df3f99a7ab
Chris <rolletchris1@gmail.com>
2025-08-26 14:20:45 -0700

    Added CooldownError to packages

--- matrix/__init__.py
+++ matrix/__init__.py
@@ -4,6 +4,7 @@ from .context import Context
 from .command import Command
 from .help import HelpCommand
 from .checks import cooldown
+from .errors import CooldownError
 
 __all__ = [
     "Bot",
@@ -12,4 +13,5 @@ __all__ = [
     "Context",
     "HelpCommand",
     "cooldown",
+    "CooldownError",
 ]
