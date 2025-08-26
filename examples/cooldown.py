39ee17626dbd37643ad186ada18e711f1a044b60
Chris <rolletchris1@gmail.com>
2025-08-26 14:20:20 -0700

    Added on_error handling for cooldown exception

--- examples/cooldown.py
+++ examples/cooldown.py
@@ -1,6 +1,7 @@
-from matrix import Bot, Context, cooldown
+from matrix import Bot, Context, cooldown, CooldownError
 
 bot = Bot("examples/config.yaml")
+
 
 @bot.command(cooldown=(2, 15))
 async def cooldown_command(ctx):
@@ -13,5 +14,11 @@ async def hi_cooldown(ctx: Context):
     print(f"{ctx.sender} invoked {ctx.body} in room {ctx.room_name}.")
     await ctx.reply("Hello World.")
 
+
+@bot.error()
+async def on_error(ctx, error):
+    if isinstance(error, CooldownError):
+        print(f"CooldownError invoked: Try again in {error.retry:.1f} seconds.")
+        await ctx.reply(f"Try again in {error.retry:.1f} seconds.")
 
 bot.start()
