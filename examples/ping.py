95d5af2af2076b27fb1e064173fc8a91ac7f28bc
Chris <rolletchris1@gmail.com>
2025-08-26 12:40:01 -0700

    remove cooldown command from ping file

--- examples/ping.py
+++ examples/ping.py
@@ -3,7 +3,6 @@ from matrix import Bot, Context, cooldown
 bot = Bot("examples/config.yaml")
 
 
-@cooldown(rate=1, period=10)
 @bot.command("ping")
 async def ping(ctx: Context):
     print(f"{ctx.sender} invoked {ctx.body} in room {ctx.room_name}.")
