0fb69ec4876bc73da0bde7c159cac8e9271ecb0e
Chris <rolletchris1@gmail.com>
2025-08-26 15:52:26 -0700

    refactor error handling on cooldown example

--- examples/cooldown.py
+++ examples/cooldown.py
@@ -8,17 +8,24 @@ async def cooldown_command(ctx):
     await ctx.reply("This is limited to 2 uses per 15s per user.")
 
 
+@bot.error()
+async def on_error(error):
+    if isinstance(error, CooldownError):
+        print(f"CooldownError invoked: Try again in {error.retry:.1f} seconds.")
+
+
 @cooldown(2, 10)
 @bot.command("hi")
-async def hi_cooldown(ctx: Context):
+async def hello(ctx: Context):
     print(f"{ctx.sender} invoked {ctx.body} in room {ctx.room_name}.")
     await ctx.reply("Hello World.")
 
 
-@bot.error()
-async def on_error(ctx, error):
+@hello.error
+async def hello_error(ctx, error):
     if isinstance(error, CooldownError):
         print(f"CooldownError invoked: Try again in {error.retry:.1f} seconds.")
         await ctx.reply(f"Try again in {error.retry:.1f} seconds.")
+
 
 bot.start()
