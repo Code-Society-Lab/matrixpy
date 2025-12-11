f0a5524291d76540a6c916c7d2b98b8eab549a32
Chris <rolletchris1@gmail.com>
2025-12-10 20:35:21 -0800

    refactor examples on shutdown
    - separate simple shutdown command with error handling

--- examples/cooldown.py
+++ examples/cooldown.py
@@ -1,7 +1,6 @@
 from matrix import Bot, Context, cooldown
-from matrix.errors import CooldownError
 
-bot = Bot("examples/config.yaml")
+bot = Bot(config="examples/config.yaml")
 
 
 @bot.command(cooldown=(2, 15))
@@ -9,23 +8,11 @@ async def cooldown_command(ctx):
     await ctx.reply("This is limited to 2 uses per 15s per user.")
 
 
-@cooldown_command.error(CooldownError)
-async def cooldown_error(ctx, error):
-    print(f"CooldownError invoked: Try again in {error.retry:.1f} seconds.")
-    await ctx.reply(f"⏳ Try again in {error.retry:.1f}s")
-
-
-@cooldown(2, 10)
-@bot.command("hi")
-async def hello(ctx: Context):
+@cooldown(rate=2, period=10)
+@bot.command("hello")
+async def hello_world(ctx: Context):
     print(f"{ctx.sender} invoked {ctx.body} in room {ctx.room_name}.")
     await ctx.reply("Hello World.")
-
-
-@hello.error(CooldownError)
-async def hello_error(ctx, error):
-    print(f"CooldownError invoked: Try again in {error.retry:.1f} seconds.")
-    await ctx.reply(f"⏳ Try again in {error.retry:.1f}s")
 
 
 bot.start()
