763d0f8acae437bb7e5157b3801f8bc8214d261b
Chris <rolletchris1@gmail.com>
2025-12-11 13:37:57 -0800

    refactor cooldown example and address PR comments

--- examples/cooldown.py
+++ examples/cooldown.py
@@ -1,18 +1,32 @@
 from matrix import Bot, Context, cooldown
+from matrix.errors import CooldownError
 
 bot = Bot(config="examples/config.yaml")
 
 
-@bot.command(cooldown=(2, 15))
-async def cooldown_command(ctx: Context) -> None:
-    await ctx.reply("This is limited to 2 uses per 15s per user.")
-
-
+# Invoke by using !hello
 @cooldown(rate=2, period=10)
 @bot.command("hello")
-async def hello_world(ctx: Context) -> None:
-    print(f"{ctx.sender} invoked {ctx.body} in room {ctx.room_name}.")
+async def hello_command(ctx: Context) -> None:
     await ctx.reply("Hello World.")
+
+
+@hello_command.error(CooldownError)
+async def hello_command_error_handler(ctx: Context, error: CooldownError) -> None:
+    print(f"CooldownError invoked: Try again in {error.retry:.1f} seconds.")
+    await ctx.reply(f"⏳ Try again in {error.retry:.1f}s")
+
+
+# Invoke by using !cooldown_command
+@bot.command(cooldown=(1, 10))
+async def cooldown_command(ctx: Context) -> None:
+    await ctx.reply("This is limited to 1 uses per 10s per user.")
+
+
+@cooldown_command.error(CooldownError)
+async def cooldown_function(ctx: Context, error: CooldownError) -> None:
+    print(f"CooldownError invoked: Try again in {error.retry:.1f} seconds.")
+    await ctx.reply(f"⏳ Try again in {error.retry:.1f}s")
 
 
 bot.start()
