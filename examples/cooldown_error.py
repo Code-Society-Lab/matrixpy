370c0985cd7d924128d9935207e295b472d1bbcf
Chris <rolletchris1@gmail.com>
2025-12-10 20:35:34 -0800

    added example of error handling of shutdown command

---
+++ examples/cooldown_error.py
@@ -0,0 +1,31 @@
+from matrix import Bot, Context, cooldown
+from matrix.errors import CooldownError
+
+bot = Bot(config="examples/config.yaml")
+
+
+# Invoke by using !hello
+@cooldown(rate=2, period=10)
+@bot.command("hello")
+async def cooldown_error(ctx: Context):
+    print(f"{ctx.sender} invoked {ctx.body} in room {ctx.room_name}.")
+    await ctx.reply("Hello World.")
+
+
+@cooldown_error.error(CooldownError)
+async def cooldown_error_handler(ctx, error):
+    print(f"CooldownError invoked: Try again in {error.retry:.1f} seconds.")
+    await ctx.reply(f"⏳ Try again in {error.retry:.1f}s")
+
+
+# Invoke by using !cooldown_command
+@bot.command(cooldown=(1, 10))
+async def cooldown_command(ctx):
+    await ctx.reply("This is limited to 1 uses per 10s per user.")
+
+@cooldown_command.error(CooldownError)
+async def cooldown_error(ctx, error):
+    print(f"CooldownError invoked: Try again in {error.retry:.1f} seconds.")
+    await ctx.reply(f"⏳ Try again in {error.retry:.1f}s")
+
+bot.start()
