9df869a5eaa3475699787746d0653130f1da6a83
penguinboi <simon.roy1211@gmail.com>
2026-03-06 01:24:38 -0500

    Add extension example

---
+++ examples/extension.py
@@ -0,0 +1,45 @@
+from matrix import Extension, Context
+
+extension = Extension("math")
+
+
+@extension.group("math", description="Math Group")
+async def math_group(ctx: Context):
+    pass
+
+
+@math_group.command()
+async def add(ctx: Context, a: int, b: int):
+    await ctx.reply(f"**{a} + {b} = {a + b}**")
+
+
+@math_group.command()
+async def subtract(ctx: Context, a: int, b: int):
+    await ctx.reply(f"{a} - {b} = {a - b}")
+
+
+@math_group.command()
+async def multiply(ctx: Context, a: int, b: int):
+    await ctx.reply(f"{a} x {b} = {a * b}")
+
+
+@math_group.command()
+async def divide(ctx: Context, a: int, b: int):
+    await ctx.reply(f"{a} ÷ {b} = {a / b}")
+
+
+@divide.error(ZeroDivisionError)
+async def divide_error(ctx: Context, error):
+    await ctx.reply(f"Divide error: {error}")
+
+
+"""
+from matrix import Bot
+from math_extension import extension as math_extension
+
+bot = Bot(config="config.yaml")
+
+
+bot.load_extension(math_extension)
+bot.start()
+"""
