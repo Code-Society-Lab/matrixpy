c50595e66511c1b0b94afee7654382518f7b5883
penguinboi <simon.roy1211@gmail.com>
2026-06-23 00:19:43 -0400

    Add get_children in Space

--- matrix/space.py
+++ matrix/space.py
@@ -1,5 +1,30 @@
+from __future__ import annotations
+
 from .room import Room
 
 
 class Space(Room):
-    pass
+    def get_children(self) -> list[Room]:
+        """Return the child rooms and spaces of this space.
+
+        ## Example
+
+        ```python
+        space = bot.get_space("!space123:matrix.org")
+
+        for child in space.get_children():
+            print(child.name)
+        ```
+        """
+        children = []
+
+        for room_id in self.children:
+            matrix_room = self._client.rooms.get(room_id)
+
+            if not matrix_room:
+                continue
+
+            room_cls = Space if matrix_room.room_type == "m.space" else Room
+            children.append(room_cls(matrix_room, self._client))
+
+        return children
