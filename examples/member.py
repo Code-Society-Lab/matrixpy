a7ef6459716ce630e75cac435893c66dd2414112
Chris <rolletchris1@gmail.com>
2026-07-08 17:39:16 -0700

    renamed example file

--- examples/member.py
+++ examples/member.py
@@ -11,11 +11,11 @@ async def profile(ctx):
     avatar = await member.get_avatar_url()
     level = await member.get_room_power_level(ctx.room)
     presence = await member.get_presence()
-    has_permission = await member.has_permission(ctx.room, "ban")
+    has_permission = await member.has_room_permission(ctx.room, "ban")
     has_event_permission = await member.has_event_permission(ctx.room, "m.room.message")
 
     await ctx.reply(
-        f"User ID: {member.user_id}\n"
+        f"Welcome {member.mention()}!\n"
         f"Display name: {name}\n"
         f"Avatar URL: {avatar}\n"
         f"Power level: {level}\n"
